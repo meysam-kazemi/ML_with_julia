a11500836c56912f00ec4148e75cbd1c5f5e6dc2
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-06 05:32:39 +0330

    add simple regression in MachineLearning.jl

---
+++ MachineLearning.jl
@@ -0,0 +1,21 @@
+import Plots , Statistics
+
+# Build a simple regression
+# The model needs to a loss function
+MSE(y,ŷ) = sum((y .- ŷ).^2) / length(y)
+
+function simple_regression(X , Y , epochs , lr = 0.01)
+    error = []
+    len = length(Y)
+    a0 , a1 = (0,0) # Coefficients
+    for e in 1:epochs # Training loop
+        # print("[$("=" ^ Int(round(200 * e/epochs)))] -> $(round((e*100/epochs),digits=2)) \r")
+        Ŷ = a0 .+ a1 .* X # Predict Y
+        # Update coefficients
+        a0 = a0 -  (lr * 2 * Statistics.mean((Ŷ - Y)))
+        a1 = a1 -  (lr * 2 * Statistics.mean((Ŷ - Y) .* X))
+        append!(error,MSE(Y,Ŷ))
+    end
+    println("Done ☑ ")
+    return a0 ,a1 , error
+end
