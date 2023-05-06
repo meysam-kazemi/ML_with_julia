0c5586897854a05151b792ec8b6c02fb0b2d420e
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-06 19:50:47 +0330

    update loop print

--- MachineLearning.jl
+++ MachineLearning.jl
@@ -9,13 +9,15 @@ function simple_regression(X , Y , epochs , lr = 0.01)
     len = length(Y)
     a0 , a1 = (0,0) # Coefficients
     for e in 1:epochs # Training loop
-        # print("[$("=" ^ Int(round(200 * e/epochs)))] -> $(round((e*100/epochs),digits=2)) \r")
+        etoepochs = e / epochs
+        print("[$("=" ^ Int(round(40 * etoepochs)) * " " ^ Int(round(40 * (1-etoepochs))))] -> $(round((e*100/epochs),digits=2)) \r")
         Ŷ = a0 .+ a1 .* X # Predict Y
         # Update coefficients
         a0 = a0 -  (lr * 2 * Statistics.mean((Ŷ - Y)))
         a1 = a1 -  (lr * 2 * Statistics.mean((Ŷ - Y) .* X))
         append!(error,MSE(Y,Ŷ))
     end
-    println("Done ☑ ")
+    # println("Done ☑ ")
     return a0 ,a1 , error
 end
+
