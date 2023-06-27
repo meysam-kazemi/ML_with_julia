c75893b495a42d25f74b80637c88cb9e73a379da
meysam-kazemi <meisam.daysu@gmail.com>
2023-06-27 07:36:28 +0330

    mutable struct SVM

--- SVM.jl
+++ SVM.jl
@@ -1,21 +1,25 @@
 
-
+mutable struct SVM
+    epoch :: Int
+    lr :: Float32
+    w :: Vector
+    output = []
+end
 
 function SVM(x,y,lr=1,epoch=100000)
-    w = zeros(size(x,2))
-    out = []
+    SVM(epoch,lr,zeros(size(x,2)))
     for e in 1:epoch
         for (i,row) in enumerate(eachrow(x))
-            val = row' * w
+            val = row' * SVM.w
             if y[i] * val < 1
-                w = w .+ lr * ((y[i] * row) .- (2*(1/epoch)*w))
+                w = w .+ lr * ((y[i] * row) .- (2*(1/epoch)*SVM.w))
             else
-                w = w .+ lr * (-2*(1/epoch)*w)
+                w = w .+ lr * (-2*(1/epoch)*SVM.w)
             end
         end
     end
     for (i,row) in enumerate(eachrow(x))
-        append!(out,row' * w)
+        append!(SVM.output,row' * SVM.w)
     end
-    return w,out
+    return SVM.w,SVM.output
 end
